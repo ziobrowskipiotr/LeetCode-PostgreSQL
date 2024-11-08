b4b56c4f5803754643da87e1710fd4190d4da9bf
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-08 14:52:13 +0100

    Time: 1336 ms (63.95%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-SELECT product_name, year, price FROM Product join Sales using(product_id);
+SELECT product_name, year, price FROM Product JOIN Sales using(product_id);
