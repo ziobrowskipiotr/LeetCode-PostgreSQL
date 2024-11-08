63e73361a517416aa8019cd1a30c38b72fa9ff4c
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-08 14:12:19 +0100

    Time: 402 ms (59.83%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-SELECT DISTINCT author_id as id from Views where author_id=viewer_id order by author_id;
+SELECT DISTINCT author_id AS id from Views where author_id=viewer_id order by author_id;
