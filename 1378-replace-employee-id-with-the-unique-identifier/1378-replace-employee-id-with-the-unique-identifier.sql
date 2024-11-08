aa696d8f4e65fa7e3c019ef1c6cbc062d340b1ce
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-08 14:46:27 +0100

    Time: 613 ms (26.62%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-SELECT unique_id, name FROM EmployeeUNI right join Employees using(id);
+SELECT unique_id, name FROM EmployeeUNI RIGHT join Employees using(id);
