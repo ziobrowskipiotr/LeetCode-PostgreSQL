f1662fe5266502c70a3afc8fe8011c47ba614ff1
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-08 16:05:22 +0100

    Time: 238 ms (77.02%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-select w1.id from Weather as w1 join Weather as w2 on w1.recordDate=w2.recordDate + interval '1 day' and w1.temperature > w2.temperature;
+SELECT w1.id from Weather as w1 join Weather as w2 on w1.recordDate=w2.recordDate + interval '1 day' and w1.temperature > w2.temperature;
