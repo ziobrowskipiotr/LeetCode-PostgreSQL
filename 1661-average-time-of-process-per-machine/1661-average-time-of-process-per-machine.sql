70bb089c8eb01cb6361754c02622d346b74ba6e9
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-10 19:10:17 +0100

    Time: 178 ms (71.87%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,13 @@
+-- Write your PostgreSQL query statement below
+select t1.machine_id, round(avg(t2.timestamp - t1.timestamp)::numeric, 3) as processing_time
+from Activity as t1 join Activity as t2
+on
+t1.machine_id=t2.machine_id
+and
+t1.process_id=t2.process_id
+and
+t1.activity_type='start'
+and
+t2.activity_type='end'
+group by
+t1.machine_id;
