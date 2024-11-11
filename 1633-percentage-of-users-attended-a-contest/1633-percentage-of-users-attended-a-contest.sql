c5fbedf40252995c027676f8f5792125bc5e5ecf
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-11 19:28:06 +0100

    Time: 1315 ms (21%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 SELECT
-contest_id, round( count(user_id) * 100.0 / (SELECT count(*) FROM Users), 2) as percentage
+contest_id, round( count(user_id) * 100.0 / (SELECT count(*) FROM Users), 2) AS percentage
 FROM
 Users join Register using(user_id)
 group by
