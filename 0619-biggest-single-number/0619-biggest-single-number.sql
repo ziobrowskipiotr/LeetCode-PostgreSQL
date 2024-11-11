67db75c6d60eb0912447292c3116eadc5198ec0c
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-11 21:39:01 +0100

    Time: 279 ms (21.95%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
 select max(num) as num
 from
-( SELECT num FROM MyNumbers GROUP BY num HAVING count(*)=1 );
+( select num FROM MyNumbers GROUP BY num HAVING count(*)=1 );
