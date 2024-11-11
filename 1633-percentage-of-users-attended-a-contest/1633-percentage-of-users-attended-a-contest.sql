ae65000863b6899961c0c834a823229e89da7d9d
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-11-11 19:28:18 +0100

    Time: 1170 ms (36.92%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -2,7 +2,7 @@
 SELECT
 contest_id, round( count(user_id) * 100.0 / (SELECT count(*) FROM Users), 2) AS percentage
 FROM
-Users join Register using(user_id)
+Users JOIN Register using(user_id)
 group by
 contest_id
 order by percentage desc, contest_id;
